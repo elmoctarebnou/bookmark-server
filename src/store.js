8e52e1155ff72c6ba52d3a7417b79ae0e403ad0a
El Moctar Ebnou <ebnoumoctar@gmail.com>
2020-08-21 21:40:41 -0500

    Bookmark server endpoints

---
+++ src/store.js
@@ -0,0 +1,26 @@
+'use strict';
+const bookmarks = [
+  {
+    id: 1,
+    title:'Google',
+    desc:'Search engine',
+    link: "http://www.google.com",
+    rating: 3
+  },
+  {
+    id: 2,
+    title:'MDN',
+    desc:'Javascript documentation',
+    link: "http://www.mdn.com",
+    rating: 4
+  },
+  {
+    id: 3,
+    title:'Tesla',
+    desc:'Electric car manufacturing companies',
+    link: "http://www.tesla.com",
+    rating: 5
+  }
+];
+
+module.exports = bookmarks;
